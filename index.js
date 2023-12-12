6e70e8d3134f86a846844b1b962d475aaff69d7a
dkiriinya <dkiriinya06sc@gmail.com>
2023-12-12 21:16:00 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+let num3 = 18 ;
+let num4 = 7 ;
+
+let mod = num3 % num4 ;
+
+let num5 = 20
+let max = Math.max(num3,num4,num5)
